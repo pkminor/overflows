89326e8275a55ad5b75ee1c1fcdaafe9aabbd16e
minorpk <minorpk@gmail.com>
2019-10-31 10:22:41 +0300

    added styling to component post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Post} from '../post';
+import {PostService} from '../post-service/post.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
 
-  constructor() { }
+export class PostComponent implements OnInit {
+  posts:Post[];
+  ls:number[]=[];
+  constructor(postservice:PostService) {   for(var i=0; i<100; i++) this.ls.push(i); 
+     this.posts=postservice.getPosts();
+  }
 
   ngOnInit() {
   }
